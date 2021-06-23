15c7ef4eeeabdfb6fbb723cc352690e26cfd701f
aaron weiner <aaronjacobweiner@yahoo.com>
2021-06-23 18:54:08 -0400

    create post, and categories seeded

--- client/src/components/Datetime.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "react-datetime/css/react-datetime.css";
-import Datetime from "react-datetime";
-
-export default function App() {
-  return <Datetime />;
-}
--- client/src/components/Postname.js
+++ client/src/components/Postname.js
@@ -1,6 +1,40 @@
-import React from 'react'
-import { Input } from 'semantic-ui-react'
+import React, { useEffect, useState } from 'react';
+import { Input } from 'semantic-ui-react';
+import "react-datetime/css/react-datetime.css";
+import Datetime from "react-datetime";
+import { DropdownButton, Dropdown } from 'react-bootstrap'
+import API from '../lib/API';
 
-const InputExampleInput = () => <Input placeholder='Enter Post Name' />
+export function Inputs(props) {
+    return (
+        <Input placeholder='Enter Post Name' {...props} />
+    );
+}
 
-export default InputExampleInput
+export function DateTime(props) {
+    return <Datetime {...props} />;
+}
+
+export function DropDownList(props) {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        loadCategories()
+    }, [])
+
+    function loadCategories() {
+        API.Categories.getCategories()
+            .then(res =>
+                setCategories(res.data)
+            )
+            .catch(err => console.log("ERROR", err));
+    };
+
+    return (
+        <DropdownButton id="dropdown-basic-button" title="Pick a Category">
+            {categories.map(item => (
+                <Dropdown.Item {...props} key={item.category_name} text={item.category_name} value={item.category_name}>{item.category_name}</Dropdown.Item>
+            ))}
+        </DropdownButton>
+    )
+}
