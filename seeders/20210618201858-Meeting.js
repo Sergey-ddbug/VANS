b655f38878b0378b64e6ea7fd1aacba05745f445
naisthorpe <aisthorpe@gmail.com>
2021-06-26 14:35:26 -0400

    Prep for pull

--- seeders/20210618201858-Meeting.js
+++ seeders/20210618201858-Meeting.js
@@ -1,14 +1,36 @@
-// 'use strict';
+'use strict';
 
-// module.exports = {
-//   up: async (queryInterface, Sequelize) => {
-//     await queryInterface.bulkInsert('People', [{
-//       name: 'John Doe',
-//       isBetaMember: false
-//     }], {});
-//   },
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.bulkInsert('Meeting', [
+            {
+                meetingName: 'Yoga with Aaron',
+                timeDate: "2021-07-07 04:00:00"
+            },
+            {
+                meetingName: 'Yoga with Nick',
+                timeDate: "2021-07-08 04:00:00"
+            },
+            {
+                meetingName: 'Yoga with Sergey',
+                timeDate: "2021-07-09 04:00:00"
+            },
+            {
+                meetingName: 'Yoga with Vika',
+                timeDate: "2021-07-10 04:00:00"
+            },
+            {
+                meetingName: 'Yoga with Caleb',
+                timeDate: "2021-07-11 04:00:00"
+            },
+            {
+                meetingName: 'Yoga with Zach',
+                timeDate: "2021-07-12 04:00:00"
+            }
+        ], {});
+    },
 
-//   down: async (queryInterface, Sequelize) => {
-//     await queryInterface.bulkDelete('People', null, {});
-//   }
-// };
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.bulkDelete('Meeting', null, {});
+    }
+};
