d76d7d8630bb2e7840acc95205d6a5c225dda86b
naisthorpe <aisthorpe@gmail.com>
2021-06-27 23:31:01 -0400

    Remove image from local server after upload

--- routes/api/images.js
+++ routes/api/images.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { User } = require('../../models');
 const upload = require('../../utilities/multer');
 const cloudinary = require('../../utilities/cloudinary');
+const fs = require('fs');
+const path = require('path');
 
 
 router.post('/', upload.single('file'), async (req, res) => {
@@ -17,6 +19,8 @@ router.post('/', upload.single('file'), async (req, res) => {
         id: req.user.id
       }
     })
+
+    fs.unlinkSync(path.join(__dirname, "../../uploads/" + req.file.filename));
 
     res.status(200).json({ status: 'ok', profileImgPublicId: cloudinaryImageData.public_id });
 
