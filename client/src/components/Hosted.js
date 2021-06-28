fa83d0858cc161e3c922e392700291a139ac4367
aaron weiner <aaronjacobweiner@yahoo.com>
2021-06-28 18:34:20 -0400

    twilio setup

--- client/src/components/Hosted.js
+++ client/src/components/Hosted.js
@@ -3,7 +3,7 @@ import API from '../lib/API';
 import { Tab, Row, Col } from 'react-bootstrap'
 
 
-function TabExampleVerticalTab() {
+function TabExampleVerticalTab({ handleSubmit }) {
   const [hostMeetings, setHostMeetings] = useState([])
 
 
@@ -33,7 +33,7 @@ function TabExampleVerticalTab() {
                       <p>{item.Category.category_name}</p>
                       <p>{item.Users[0].first_name}</p>
                       <p>{item.timeDate}</p>
-                      <button>Join</button>
+                      <button onClick={(e) => handleSubmit(e, item.meetingName)}>Join</button>
                       <button>Delete</button>
                     </div>
                   ))}
