81a8189b8e6e943f31517a26f7127702b7997b33
vmidor <76964172+vmidor@users.noreply.github.com>
2021-07-08 22:22:02 -0400

    change hidden models, media screen and added pic

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -48,94 +48,92 @@ function TabExampleVerticalTab() {
 
 
     return (
-        <div className="test3">
-            <ul>
-                <Container className="categoriesConteiner">
+        // <div className="test3">
+        <Container className="categoriesConteiner test3">
 
 
-                    <Tab.Container id="left-tabs-example" defaultActiveKey="all" >
-                        <Row>
-                            <Col sm={3}>
-                                <Nav variant="pills" className="flex-column">
-                                    <Nav.Item>
-                                        <Nav.Link eventKey="all">All</Nav.Link>
-                                    </Nav.Item>
-                                    {categories.map(item => (
-                                        <Nav.Item >
-                                            <Nav.Link key={item.id} eventKey={item.id}>{item.category_name}</Nav.Link>
-                                        </Nav.Item>
-                                    ))}
+            <Tab.Container id="left-tabs-example" defaultActiveKey="all" >
+                <Row>
+                    <Col md={3}>
+                        <Nav variant="pills" className="flex-column">
+                            <Nav.Item>
+                                <Nav.Link eventKey="all">All</Nav.Link>
+                            </Nav.Item>
+                            {categories.map(item => (
+                                <Nav.Item >
+                                    <Nav.Link key={item.id} eventKey={item.id}>{item.category_name}</Nav.Link>
+                                </Nav.Item>
+                            ))}
 
 
-                                </Nav>
-                            </Col>
+                        </Nav>
+                    </Col>
 
-                            <Col sm={9}>
-                                <Tab.Content>
-                                    <Tab.Pane eventKey="all">
-                                        {
-                                            (meetings.length > 0)
-                                                ?
-                                                (meetings.map(item => (
-                                                    <div data-id={item.id} className="w-100 border d-flex flex-row justify-content-between m-3 p-3">
-                                                        <h3>{item.meetingName}</h3>
-                                                        <p>{item.Category.category_name}</p>
-                                                        <p>{item.Users[0].first_name}</p>
-                                                        <p>{item.timeDate}</p>
-
-
-                                                        {item.isUserMeeting ? (
-                                                            <button
-                                                                onClick={() => console.log('DO REMOVE')}
-                                                                className="btn btn-danger btn-md"
-                                                            >
-                                                                Remove
-                                                            </button>) : (
-                                                                <button
-                                                                    onClick={handleJoinBtnClick}
-                                                                    className="btn btn-danger btn-md"
-                                                                >
-                                                                    Add
-                                                                </button>
-                                                            )}
-                                                    </div>
-                                                )))
-                                                :
-                                                (<h2>Nothing to see here</h2>)
-                                        }
-                                    </Tab.Pane>
-                                    {meetings.map(item => (
-                                        //TODO: need to have code to check database to see if user is already linked to the meeting and if so ---return
-                                        <Tab.Pane key={item.id} eventKey={item.CategoryId}>
-                                            <div data-id={item.id} className="w-100 border d-flex flex-row justify-content-between m-3 p-3">
+                    <Col md={9}>
+                        <Tab.Content>
+                            <Tab.Pane eventKey="all">
+                                {
+                                    (meetings.length > 0)
+                                        ?
+                                        (meetings.map(item => (
+                                            <div data-id={item.id} className="w-100 border d-flex flex-column flex-md-row justify-content-between mt-3 mb-3 pl-3 pr-3">
                                                 <h3>{item.meetingName}</h3>
                                                 <p>{item.Category.category_name}</p>
                                                 <p>{item.Users[0].first_name}</p>
                                                 <p>{item.timeDate}</p>
-                                                {item.isUserMeeting
-                                                    ?
-                                                    (
-                                                        <button onClick={() => console.log('DO REMOVE')} className="btn btn-danger btn-md">
-                                                            Remove
-                                                        </button>
-                                                    )
-                                                    :
-                                                    (
-                                                        <button onClick={handleJoinBtnClick} className="btn btn-danger btn-md">
+
+
+                                                {item.isUserMeeting ? (
+                                                    <button
+                                                        onClick={() => console.log('DO REMOVE')}
+                                                        className="btn btn-danger btn-md mb-2"
+                                                    >
+                                                        Remove
+                                                    </button>) : (
+                                                        <button
+                                                            onClick={handleJoinBtnClick}
+                                                            className="btn btn-danger btn-md mb-2"
+                                                        >
                                                             Add
                                                         </button>
-                                                    )
-                                                }
+                                                    )}
                                             </div>
-                                        </Tab.Pane>
-                                    ))}
-                                </Tab.Content>
-                            </Col>
-                        </Row>
-                    </Tab.Container>
-                </Container>
-            </ul>
-        </div >
+                                        )))
+                                        :
+                                        (<h2>Nothing to see here</h2>)
+                                }
+                            </Tab.Pane>
+                            {meetings.map(item => (
+                                //TODO: need to have code to check database to see if user is already linked to the meeting and if so ---return
+                                <Tab.Pane key={item.id} eventKey={item.CategoryId}>
+                                    <div data-id={item.id} className="w-100 border d-flex flex-row justify-content-between m-3 p-3">
+                                        <h3>{item.meetingName}</h3>
+                                        <p>{item.Category.category_name}</p>
+                                        <p>{item.Users[0].first_name}</p>
+                                        <p>{item.timeDate}</p>
+                                        {item.isUserMeeting
+                                            ?
+                                            (
+                                                <button onClick={() => console.log('DO REMOVE')} className="btn btn-danger btn-md">
+                                                    Remove
+                                                </button>
+                                            )
+                                            :
+                                            (
+                                                <button onClick={handleJoinBtnClick} className="btn btn-danger btn-md">
+                                                    Add
+                                                </button>
+                                            )
+                                        }
+                                    </div>
+                                </Tab.Pane>
+                            ))}
+                        </Tab.Content>
+                    </Col>
+                </Row>
+            </Tab.Container>
+        </Container>
+        // {/* </div > */ }
     );
 
 }
