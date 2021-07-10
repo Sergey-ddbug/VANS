e81e027348c91f375936cdf671e7b054c3e49d04
aaron weiner <aaronjacobweiner@yahoo.com>
2021-07-09 20:33:29 -0400

    jd

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { FaReact, FaBootstrap, FaVideo, FaDatabase} from "react-icons/fa";
+import { FaReact, FaBootstrap, FaVideo, FaDatabase } from "react-icons/fa";
 import '../pages/assets/Login.css';
 
 const Footer = () => <footer className="page-footer font-small blue pt-4">
@@ -8,8 +8,8 @@ const Footer = () => <footer className="page-footer font-small blue pt-4">
       <div className="col-md-6 mt-md-0 mt-3">
         <h5 className="text-uppercase text-danger">Video Audio Networking Service</h5>
         <p>Welcome to the VANS family. Learn the latest skills to reach your professional goals.
-        <p>Online video courses with new additions published every week.
-        </p>
+          <p>Online video courses with new additions published every week.
+          </p>
         </p>
       </div>
 
@@ -29,17 +29,17 @@ const Footer = () => <footer className="page-footer font-small blue pt-4">
       <div className="col-md-3 mb-md-0 mb-3">
         <h5 className="text-uppercase text-danger icons">We Use</h5>
         <ul>
-          <a><FaReact/></a>
-          <a><FaBootstrap/></a>
-          <a><FaVideo/></a>
-          <a><FaDatabase/></a>
-          </ul>
+          <a className="mr-1"><FaReact /></a>
+          <a className="mr-1"><FaBootstrap /></a>
+          <a className="mr-1"><FaVideo /></a>
+          <a><FaDatabase /></a>
+        </ul>
       </div>
     </div>
   </div>
-  <div className="footer-copyright text-center py-3">© VANS 2021 
+  <div className="footer-copyright text-center py-3">© VANS 2021
 
-    </div>
+  </div>
 
 </footer>
 
